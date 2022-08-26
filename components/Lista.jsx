3fd0bdb1e1d261fe5a303b06fe24cceebba40398
patricklouback <patrickmlouback@gmail.com>
2022-08-26 14:38:01 -0300

    componentes filhos

---
+++ components/Lista.jsx
@@ -0,0 +1,8 @@
+export default function Lista(props) {
+    return (
+        <ul>
+            <h1>Lista</h1>
+            {props.children}
+        </ul>
+    )
+}
---
+++ components/SomentePar.jsx
@@ -0,0 +1,4 @@
+export default function SomentePar(props) {
+
+    return props.numero % 2 === 0 ? <h1>{props.numero}</h1> : null
+}
